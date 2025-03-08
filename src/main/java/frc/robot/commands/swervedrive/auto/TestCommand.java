0b519d2d8b12fe3599b8c9d9aed34d7ebcaef660
Ian <killohurtzz@gmail.com>
2025-03-08 15:12:08 -0500

    Testing Group Commands

--- src/main/java/frc/robot/commands/swervedrive/auto/TestCommand.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/TestCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.swervedrive.auto;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
@@ -9,5 +10,6 @@ public class TestCommand extends Command
     public TestCommand()
     {
         System.out.println("It worked!");
+        DriverStation.reportError(getName(), null);
     }
 }
