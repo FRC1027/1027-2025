9c4d26433a397939f30f60af45c6137b6630b6ae
Ian <killohurtzz@gmail.com>
2025-03-10 19:32:48 -0400

    Revert "Autonomous Testing"
    This reverts commit 8109e9af47cb3bf21331a1133163d655432e5ed0.

---
+++ src/main/java/frc/robot/commands/swervedrive/auto/TestCommand.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.swervedrive.auto;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+
+public class TestCommand extends Command
+{
+    public TestCommand()
+    {
+        System.out.println("It worked!");
+        DriverStation.reportError(getName(), null);
+    }
+}
