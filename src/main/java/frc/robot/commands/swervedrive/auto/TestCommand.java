c007d9bc39aa73eace6c6e251500050c8e654b2f
Ian <killohurtzz@gmail.com>
2025-03-11 21:05:26 -0400

    Auto Testing

--- src/main/java/frc/robot/commands/swervedrive/auto/TestCommand.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/TestCommand.java
@@ -1,12 +1,18 @@
 package frc.robot.commands.swervedrive.auto;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 
 public class TestCommand extends Command
 {
     public TestCommand()
-    {
+
+        
 
         //System.out.println("It worked!");
         //DriverStation.reportError(getName(), null);
