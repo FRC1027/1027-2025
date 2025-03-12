88aa6f511e42a36fe279fd754289fe49c8de0e15
Ian <killohurtzz@gmail.com>
2025-03-11 20:19:00 -0400

    BLEHHHHH :(

--- src/main/java/frc/robot/commands/swervedrive/auto/TestCommand.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/TestCommand.java
@@ -1,15 +1,15 @@
 package frc.robot.commands.swervedrive.auto;
 
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 
 public class TestCommand extends Command
 {
     public TestCommand()
     {
-        System.out.println("It worked!");
+
+        //System.out.println("It worked!");
         //DriverStation.reportError(getName(), null);
+        new PrintCommand("Maybe?");
     }
 }
