38ddb9179cf461206fd5c6447539edee46be3b1a
MarkLittlejohn <86631825+MarkLittlejohn@users.noreply.github.com>
2025-03-11 21:46:41 -0400

    Update TestCommand.java

--- src/main/java/frc/robot/commands/swervedrive/auto/TestCommand.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/TestCommand.java
@@ -10,12 +10,22 @@ import edu.wpi.first.wpilibj2.command.PrintCommand;
 
 public class TestCommand extends Command
 {
-    public TestCommand()
-
-        
-
+    public Command TestCommandAuto()        
+    {
         //System.out.println("It worked!");
         //DriverStation.reportError(getName(), null);
         new PrintCommand("Maybe?");
+        try{
+            // Load the path you want to follow using its name in the GUI
+            PathPlannerPath path = PathPlannerPath.fromPathFile("Position1(R3)Coral1");
+    
+            // Create a path following command using AutoBuilder. This will also trigger event markers.
+            return AutoBuilder.followPath(path);
+        } catch (Exception e) {
+            DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
+            return Commands.none();
+        } finally {
+            new PrintCommand("It worked!");
+        }
     }
 }
