c57300795aab998d23300df155bf6fb17420d69b
MarkLittlejohn <86631825+MarkLittlejohn@users.noreply.github.com>
2025-11-21 09:21:08 -0500

    Object Recognition Take X
    Removed debug lines and increased ms wait time (we may want to reduce to 50ms once we confirm pipeline switching works).

--- src/main/java/frc/robot/subsystems/ObjectRecognition.java
+++ src/main/java/frc/robot/subsystems/ObjectRecognition.java
@@ -17,16 +17,11 @@ public class ObjectRecognition extends SubsystemBase{
         LimelightHelpers.setPipelineIndex("limelight", 1);
 
         // wait for LL to actually process the new pipeline
-        // Limelight runs at 22ms/frame meaning 50ms wait guarantees at least 2 frames processed.
-        try { Thread.sleep(50); } catch (Exception e) {}
+        // Limelight runs at 22ms/frame meaning 100ms wait guarantees at least 4 frames processed.
+        try { Thread.sleep(100); } catch (Exception e) {}
 
         LimelightResults results = LimelightHelpers.getLatestResults("limelight");
         
-        System.out.println(results); //frc.robot.LimelightHelpers$LimelightResults@1d1f8dd
-        System.out.println(results.targets_Detector); //[Lfrc.robot.LimelightHelpers$LimelightTarget_Detector;@96b474
-        System.out.println(results.targets_Detector.length); //0
-        //System.out.println(results.targets_Detector[0].className); //crashes robot code if length is 0
-
         // Neural network detections
         if (results.targets_Detector.length > 0) {
             LimelightTarget_Detector detection = results.targets_Detector[0];
